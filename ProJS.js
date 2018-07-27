384822b2fee26098af1cbc6ba6a38487de013f7b
D'Jreya Boyd <37115281+JreyaKyong@users.noreply.github.com>
2018-07-26 20:07:38 -0400

    Added If Statement
    Added an if statement to the js file. Added a link to jquery into the html code.

--- ProJS.js
+++ ProJS.js
@@ -1,1 +1,11 @@
-$(".name").css("color", "green")
+//window resize based on browser event
+$(window).resize(function(){
+	if ($(window).width() <= 800){
+		// do something here
+  $("#name").css("color", "green");
+	}
+  if ($(window).width() > 800){
+    // do something here
+  $("#name").css("color", "red");
+}
+});
